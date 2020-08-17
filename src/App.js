19c6255f7e4f67031ad120ddd556273deaeda663
Matheus Zenker <matheus.g.zenker@gmail.com>
2020-08-16 21:46:30 -0300

    First classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import kanban from "./constants";
 
 function App() {
+  const dragEnd = (e) => {
+    console.log("DragEnd", e);
+  };
+
+  console.log(kanban);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DragDropContext onDragEnd={(e) => dragEnd(e)}>
+          <div className="container">
+            <Droppable key="allCards" droppableId="allCards">
+              {(provided, snapshot) => (
+                <div className="list-container">
+                  {kanban.forEach((index, list) => (
+                    <div className="list" key={index}>
+                      meu teste
+                    </div>
+                  ))}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        </DragDropContext>
       </header>
     </div>
   );
---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: "list-1",
+  },
+  {
+    id: "list-2",
+  },
+  {
+    id: "list-3",
+  },
+  {
+    id: "list-4",
+  },
+  {
+    id: "list-5",
+  },
+  {
+    id: "list-6",
+  },
+  {
+    id: "list-7",
+  },
+  {
+    id: "list-8",
+  },
+];
